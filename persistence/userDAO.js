db6032af0933322e7f19bd8b8b2b1182749aa2c7
Julio Sandroni <x234665@prservicos.com.br>
2020-03-29 11:42:43 -0300

    verify user exists

--- persistence/userDAO.js
+++ persistence/userDAO.js
@@ -1,22 +1,35 @@
-function userDAO(db){
-  this.db = db
+function userDAO(db) {
+  this.db = db;
 }
 
-userDAO.prototype.insert = function(user){
-    let listUsers = this.db.get('users')
+userDAO.prototype.insert = function(user) {
+  let listUsers = this.db.get("users");
 
-    if(!listUsers){
-      listUsers = []
-    }
-    
-    let id = listUsers.length
-    user.id = (id+1)
-    
-    listUsers.push(user)
-    this.db.set('users',listUsers)
+  if (!listUsers) {
+    listUsers = [];
+  }
+
+  let id = listUsers.length;
+  user.id = id + 1;
+
+  listUsers.push(user);
+  this.db.set("users", listUsers);
+};
+
+userDAO.prototype.has = function(user) {
+  const listUsers = this.db.get("users");
+
+  if(!listUsers){
+    return false
+  }
+
+  let result = listUsers.filter(item => {
+    return item.email == user.email || item.cpf == user.cpf;
+  })
+
+  return result.length > 0
 }
 
-
-module.exports = function () {
-  return userDAO
+module.exports = function() {
+  return userDAO;
 }
--- routers/user.js
+++ routers/user.js
@@ -1,33 +1,44 @@
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require("express-validator");
+const sha1 = require("sha1");
 
 module.exports = app => {
   app.post(
-    '/user',
+    "/user",
     [
-      check('nome')
-      .isLength(5)
-      .withMessage('Nome inválido'),
-      check('cpf')
+      check("nome")
+        .isLength(5)
+        .withMessage("Nome inválido"),
+      check("cpf")
         .isLength(11)
-        .withMessage('Número de CPF inválido'),
-      check('email')
+        .withMessage("Número de CPF inválido"),
+      check("email")
         .isEmail()
-        .withMessage('Informe um e-mail válido'),
-      check('senha')
+        .withMessage("Informe um e-mail válido"),
+      check("senha")
         .isLength(8)
-        .withMessage('Informe uma senha com pelo menos 8 Digitos')
+        .withMessage("Informe uma senha com pelo menos 8 Digitos")
     ],
     (req, res, next) => {
       const errors = validationResult(req);
+
       if (!errors.isEmpty()) {
-        console.log(errors)
+        console.log(errors);
         return res.status(422).json({ errors: errors.array() });
       }
 
+      let user = req.body;
+      user.senha = sha1(user.senha);
+
       const db = app.persistence.connection;
       const userDAO = new app.persistence.userDAO(db);
-      userDAO.insert(req.body);
-      res.status(200).send({message:'sucess'});
+
+      if (userDAO.has(user)) {
+        res.status(401).send({ message: 'CPF ou e-mail já cadastrado' })
+        return
+      }
+
+      userDAO.insert(user);
+      res.status(200).send({ message: "sucess" });
     }
   );
 };
