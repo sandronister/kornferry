04653b64c7f737d951267cd5c42ef6b94f3f347c
Julio Sandroni <x234665@prservicos.com.br>
2020-03-29 11:08:25 -0300

    validators

--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,33 @@
+const { check, validationResult } = require('express-validator');
+
 module.exports = app => {
-  app.post("/user", (req, res, next) => {
-    const db = app.persistence.connection;
-    const userDAO = new app.persistence.userDAO(db);
-    console.log(req.body)
-    userDAO.insert(req.body);
-    res.status(200).send('ok')
-  });
+  app.post(
+    '/user',
+    [
+      check('nome')
+      .isLength(5)
+      .withMessage('Nome inválido'),
+      check('cpf')
+        .isLength(11)
+        .withMessage('Número de CPF inválido'),
+      check('email')
+        .isEmail()
+        .withMessage('Informe um e-mail válido'),
+      check('senha')
+        .isLength(8)
+        .withMessage('Informe uma senha com pelo menos 8 Digitos')
+    ],
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.log(errors)
+        return res.status(422).json({ errors: errors.array() });
+      }
+
+      const db = app.persistence.connection;
+      const userDAO = new app.persistence.userDAO(db);
+      userDAO.insert(req.body);
+      res.status(200).send({message:'sucess'});
+    }
+  );
 };
